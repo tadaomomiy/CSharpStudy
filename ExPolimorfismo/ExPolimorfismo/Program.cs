f123719078ece740b2954531c3244c8520308bd2
Matheus Tadao Momiy <matheus.tadao@gmail.com>
2024-09-25 14:42:26 -0300

    Add files via upload

---
+++ ExPolimorfismo/ExPolimorfismo/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExPolimorfismo.Entities;
+using System.Globalization;
+
+namespace ExPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char outsourcedAnswer = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string employeeName = Console.ReadLine();
+                Console.Write("Hours: ");
+                int employeeWorkedHours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double employeeValuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (outsourcedAnswer != 'n')
+                {
+                    Console.Write("Additional charge: ");
+                    double outsourceAdditionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    employees.Add(new OutsourcedEmployee(employeeName, employeeWorkedHours, employeeValuePerHour, outsourceAdditionalCharge)) ;
+                } else
+                {
+                    employees.Add(new Employee(employeeName, employeeWorkedHours, employeeValuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.Name + " - $ " + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ ExercicioPolimorfismoFix/ExercicioPolimorfismoFix/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExercicioPolimorfismoFix.Entities;
+using System.Globalization;
+
+namespace ExercicioPolimorfismoFix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char productType = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (productType)
+                {
+                    case 'i':
+                        Console.Write("Customs fee: ");
+                        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        products.Add(new ImportedProduct(productName, productPrice, customsFee));
+                        break;
+                    case 'u':
+                        Console.Write("Manufacture date (DD/MM/YYYY): ");
+                        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                        products.Add(new UsedProduct(productName, productPrice, manufactureDate));
+                        break;
+                    default:
+                        products.Add(new Product(productName, productPrice));
+                        break;
+
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
