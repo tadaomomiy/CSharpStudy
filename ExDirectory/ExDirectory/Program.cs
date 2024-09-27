25be60cc9efd6b9cdfc237b8aa8d3839a0e1f889
Matheus Tadao Momiy <matheus.tadao@gmail.com>
2024-09-27 12:29:41 -0300

    Add files via upload

---
+++ ExDirectory/ExDirectory/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine();
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (Exception ex) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExercicioStream/ExercicioStream/Program.cs
@@ -0,0 +1,74 @@
+﻿using ExercicioStream.Entities;
+using System.Globalization;
+
+namespace ExercicioStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"C:\temp\myFolder\newfolder";
+            //string filePath = @"C:\temp\myFolder\newfolder\product.csv"; // Mine
+
+            Console.WriteLine("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+
+                    //Directory.CreateDirectory(path + @"\out");
+
+                    //string targetPath = path + @"\out\summary.csv";
+
+                    //using (StreamReader sr = new StreamReader(filePath))
+                    //{
+                    //    while (!sr.EndOfStream)
+                    //    {
+                    //        string line = sr.ReadLine();
+                    //        string[] cells = line.Split(',');
+                    //        double price = double.Parse(cells[1], CultureInfo.InvariantCulture);
+                    //        int quantity = int.Parse(cells[2]);
+
+                    //        double totalValue = price * quantity;
+
+                    //        using (StreamWriter sw = File.AppendText(targetPath))
+                    //        {
+                    //            sw.WriteLine(cells[0] + "," + totalValue.ToString("F2", CultureInfo.InvariantCulture));
+                    //        }
+                    //    }
+                    //}
+                    //Console.WriteLine("Success"); //Mine
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
